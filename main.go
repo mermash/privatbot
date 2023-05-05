65ac36dd6a13c2b849a12d42a61500a23a4ca7ed
merkulova <merkulova.maria.1990@gmail.com>
2023-05-05 16:19:58 +0300

    fix command

--- main.go
+++ main.go
@@ -163,7 +163,7 @@ func main() {
 	privatURL := os.Getenv("PRIVAT_CASH_CURRENCY_API_URL")
 
 	var bank map[string]string = map[string]string{
-		"Privat": privatURL,
+		"privat": privatURL,
 	}
 
 	bot, err := tgbotapi.NewBotAPI(botToken)
@@ -199,7 +199,7 @@ func main() {
 		} else {
 			bot.Send(tgbotapi.NewMessage(
 				update.Message.Chat.ID,
-				"there is only privat exchange rate",
+				"There is only \"privat\" exchange rate. Please, type the \"privat\"",
 			))
 		}
 	}
